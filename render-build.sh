216a4e744f0cc2e9f62fbcf445b3d2e2ba00f13b
RajuM1997 <mraju5797@gmail.com>
2025-12-31 12:03:54 +0600

    first commit

---
+++ render-build.sh
@@ -0,0 +1,5 @@
+set -o errexit
+bun install
+bun run build
+npx prisma generate
+npx prisma migrate deploy
---
+++ src/helpers/jwtHelpers.ts
@@ -0,0 +1,21 @@
+import jwt, { JwtPayload, Secret, SignOptions } from "jsonwebtoken";
+
+const generateToken = (payload: any, secret: Secret, expiresIn: string) => {
+  console.log(secret);
+
+  const token = jwt.sign(payload, secret, {
+    algorithm: "HS256",
+    expiresIn,
+  } as SignOptions);
+
+  return token;
+};
+
+const verifyToken = (token: string, secret: Secret) => {
+  return jwt.verify(token, secret) as JwtPayload;
+};
+
+export const jwtHelpers = {
+  generateToken,
+  verifyToken,
+};
